8224eed2eb2d5bd169b65e54f8fe90a5d370f36b
Zheng Shao <axot@msn.com>
2015-02-26 02:17:57 +0900

    Initial Commit

---
+++ iSstp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iSstp
+//
+//  Created by Zheng Shao on 2/26/15.
+//  Copyright (c) 2015 axot. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ iSstp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iSstp
+//
+//  Created by Zheng Shao on 2/26/15.
+//  Copyright (c) 2015 axot. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
